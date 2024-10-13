7c7a95318a8ca72824bf4b51258c3b8fd62d0a46
learnitlessons <162319132+learnitlessons@users.noreply.github.com>
2024-10-13 14:33:10 +0300

    Update add-lab-adds.ps1

--- add-lab-adds.ps1
+++ add-lab-adds.ps1
@@ -57,7 +57,20 @@ function Configure-PagefileAndRestart {
     Start-Sleep -Seconds 300
 }
 
+# Configure pagefile and restart lon-dc1
+Configure-PagefileAndRestart -resourceGroup 'rg-lit-ADLab-ukw' -vmName 'lon-dc1'
 
+# Promote lon-dc1 as first DC in learnitlessons.com
+$script = @"
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+Import-Module ADDSDeployment
+`$securePassword = ConvertTo-SecureString '$password' -AsPlainText -Force
+Install-ADDSForest -CreateDnsDelegation:`$false -DatabasePath 'C:\Windows\NTDS' -DomainMode 'WinThreshold' -DomainName 'learnitlessons.com' -DomainNetbiosName 'LIT' -ForestMode 'WinThreshold' -InstallDns:`$true -LogPath 'C:\Windows\NTDS' -NoRebootOnCompletion:`$false -SysvolPath 'C:\Windows\SYSVOL' -Force:`$true -SafeModeAdministratorPassword `$securePassword
+"@
+Run-AzVMCommand -resourceGroup 'rg-lit-ADLab-ukw' -vmName 'lon-dc1' -script $script
+
+Write-Host "Waiting for forest to be ready (10 minutes)..."
+Start-Sleep -Seconds 600
 
 # Configure pagefile and restart lon-dc2
 Configure-PagefileAndRestart -resourceGroup 'rg-lit-ADLab-ukw' -vmName 'lon-dc2'
