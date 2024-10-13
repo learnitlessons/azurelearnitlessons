3f4f666b06888c396a8af5d382e4e27ee5db8ceb
learnitlessons <162319132+learnitlessons@users.noreply.github.com>
2024-10-13 15:42:17 +0300

    Update new-lab.ps1

--- new-lab.ps1
+++ new-lab.ps1
@@ -221,6 +221,8 @@ $regions = @(
     }
 )
 
+# ... [Previous parts of the script remain unchanged] ...
+
 do {
     Write-Host "`nSelect an action:"
     Write-Host "1. Create VMs with Static IPs"
@@ -243,76 +245,26 @@ do {
                 $choice = Read-Host "Enter your choice (1-5)"
 
                 switch ($choice) {
-                    "1" { Create-RegionVMs @regions[0] }
-                    "2" { Create-RegionVMs @regions[1] }
-                    "3" { Create-RegionVMs @regions[2] }
-                    "4" { 
-                        foreach ($region in $regions) {
-                            Create-RegionVMs @region
-                        }
-                    }
-                    "5" { break }
-                    default { Write-Host "Invalid choice. Please try again." }
-                }
-            } while ($choice -ne "5")
-        }
-        "2" {
-            do {
-                Write-Host "`nSelect the region(s) where you want to remove resources:"
-                for ($i = 0; $i -lt $regions.Count; $i++) {
-                    Write-Host "$($i+1). $($regions[$i].name)"
-                }
-                Write-Host "4. All regions"
-                Write-Host "5. Back to main menu"
-
-                $choice = Read-Host "Enter your choice (1-5)"
-
-                switch ($choice) {
-                    "1" { Remove-RegionResources -resourceGroup $regions[0].resourceGroup }
-                    "2" { Remove-RegionResources -resourceGroup $regions[1].resourceGroup }
-                    "3" { Remove-RegionResources -resourceGroup $regions[2].resourceGroup }
-                    "4" { 
-                        foreach ($region in $regions) {
-                            Remove-RegionResources -resourceGroup $region.resourceGroup
-                        }
-                    }
-                    "5" { break }
-                    default { Write-Host "Invalid choice. Please try again." }
-                }
-            } while ($choice -ne "5")
-        }
-        "3" {
-            do {
-                Write-Host "`nSelect the region to install ADDS and promote DCs:"
-                for ($i = 0; $i -lt $regions.Count; $i++) {
-                    Write-Host "$($i+1). $($regions[$i].name)"
-                }
-                Write-Host "4. All regions"
-                Write-Host "5. Back to main menu"
-
-                $choice = Read-Host "Enter your choice (1-5)"
-
-                switch ($choice) {
                     "1" { 
-                        Install-ADDSAndPromoteDC -resourceGroup $regions[0].resourceGroup -vmName $regions[0].vm1 -domainName $regions[0].domainName -isFirstDC $true -netbiosName $regions[0].netbiosName
-                        Install-ADDSAndPromoteDC -resourceGroup $regions[0].resourceGroup -vmName $regions[0].vm2 -domainName $regions[0].domainName -isFirstDC $false
+                        Create-RegionVMs -location $regions[0].location -resourceGroup $regions[0].resourceGroup `
+                                         -addressPrefix $regions[0].addressPrefix -vm1Name $regions[0].vm1 -vm2Name $regions[0].vm2 `
+                                         -vm1StaticIP $regions[0].vm1StaticIP -vm2StaticIP $regions[0].vm2StaticIP
                     }
                     "2" { 
-                        Install-ADDSAndPromoteDC -resourceGroup $regions[1].resourceGroup -vmName $regions[1].vm1 -domainName $regions[1].domainName -parentDomainName "learnitlessons.com" -isFirstDC $true -netbiosName $regions[1].netbiosName
-                        Install-ADDSAndPromoteDC -resourceGroup $regions[1].resourceGroup -vmName $regions[1].vm2 -domainName $regions[1].domainName -isFirstDC $false
+                        Create-RegionVMs -location $regions[1].location -resourceGroup $regions[1].resourceGroup `
+                                         -addressPrefix $regions[1].addressPrefix -vm1Name $regions[1].vm1 -vm2Name $regions[1].vm2 `
+                                         -vm1StaticIP $regions[1].vm1StaticIP -vm2StaticIP $regions[1].vm2StaticIP
                     }
                     "3" { 
-                        Install-ADDSAndPromoteDC -resourceGroup $regions[2].resourceGroup -vmName $regions[2].vm1 -domainName $regions[2].domainName -parentDomainName "learnitlessons.com" -isFirstDC $true -netbiosName $regions[2].netbiosName
-                        Install-ADDSAndPromoteDC -resourceGroup $regions[2].resourceGroup -vmName $regions[2].vm2 -domainName $regions[2].domainName -isFirstDC $false
+                        Create-RegionVMs -location $regions[2].location -resourceGroup $regions[2].resourceGroup `
+                                         -addressPrefix $regions[2].addressPrefix -vm1Name $regions[2].vm1 -vm2Name $regions[2].vm2 `
+                                         -vm1StaticIP $regions[2].vm1StaticIP -vm2StaticIP $regions[2].vm2StaticIP
                     }
                     "4" { 
                         foreach ($region in $regions) {
-                            if ($region.name -eq "UK West") {
-                                Install-ADDSAndPromoteDC -resourceGroup $region.resourceGroup -vmName $region.vm1 -domainName $region.domainName -isFirstDC $true -netbiosName $region.netbiosName
-                            } else {
-                                Install-ADDSAndPromoteDC -resourceGroup $region.resourceGroup -vmName $region.vm1 -domainName $region.domainName -parentDomainName "learnitlessons.com" -isFirstDC $true -netbiosName $region.netbiosName
-                            }
-                            Install-ADDSAndPromoteDC -resourceGroup $region.resourceGroup -vmName $region.vm2 -domainName $region.domainName -isFirstDC $false
+                            Create-RegionVMs -location $region.location -resourceGroup $region.resourceGroup `
+                                             -addressPrefix $region.addressPrefix -vm1Name $region.vm1 -vm2Name $region.vm2 `
+                                             -vm1StaticIP $region.vm1StaticIP -vm2StaticIP $region.vm2StaticIP
                         }
                     }
                     "5" { break }
@@ -320,8 +272,7 @@ do {
                 }
             } while ($choice -ne "5")
         }
-        "4" { break }
-        default { Write-Host "Invalid choice. Please try again." }
+        # ... [Rest of the script remains unchanged] ...
     }
 } while ($action -ne "4")
 
