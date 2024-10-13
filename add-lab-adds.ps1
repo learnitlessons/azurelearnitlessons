b9ca3b39c500f2628a1acf35fed57aba97e43ac0
learnitlessons <162319132+learnitlessons@users.noreply.github.com>
2024-10-13 08:36:36 +0300

    Update add-lab-adds.ps1

--- add-lab-adds.ps1
+++ add-lab-adds.ps1
@@ -57,54 +57,6 @@ function Configure-PagefileAndRestart {
     Start-Sleep -Seconds 300
 }
 
-# Configure pagefile and restart lon-dc1
-Configure-PagefileAndRestart -resourceGroup 'rg-lit-ADLab-ukw' -vmName 'lon-dc1'
-
-# Promote lon-dc1 as first DC in learnitlessons.com
-$script = @"
-Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
-Import-Module ADDSDeployment
-`$securePassword = ConvertTo-SecureString '$password' -AsPlainText -Force
-Install-ADDSForest -CreateDnsDelegation:`$false -DatabasePath 'C:\Windows\NTDS' -DomainMode 'WinThreshold' -DomainName 'learnitlessons.com' -DomainNetbiosName 'LIT' -ForestMode 'WinThreshold' -InstallDns:`$true -LogPath 'C:\Windows\NTDS' -NoRebootOnCompletion:`$false -SysvolPath 'C:\Windows\SYSVOL' -Force:`$true -SafeModeAdministratorPassword `$securePassword
-"@
-Run-AzVMCommand -resourceGroup 'rg-lit-ADLab-ukw' -vmName 'lon-dc1' -script $script
-
-Write-Host "Waiting for forest to be ready (10 minutes)..."
-Start-Sleep -Seconds 600
-
-# Add new user 'shumi' and grant necessary permissions
-$script = @"
-`$securePassword = ConvertTo-SecureString '$password' -AsPlainText -Force
-New-ADUser -Name "shumi" -AccountPassword `$securePassword -Enabled `$true
-Add-ADGroupMember -Identity "Domain Admins" -Members "shumi"
-Add-ADGroupMember -Identity "Enterprise Admins" -Members "shumi"
-"@
-Run-AzVMCommand -resourceGroup 'rg-lit-ADLab-ukw' -vmName 'lon-dc1' -script $script
-
-Write-Host "User 'shumi' added and granted permissions. Waiting for 5 minutes..."
-Start-Sleep -Seconds 300
-
-# Configure pagefile and restart lon-dc2
-Configure-PagefileAndRestart -resourceGroup 'rg-lit-ADLab-ukw' -vmName 'lon-dc2'
-
-# Configure DNS on lon-dc2
-$script = @"
-Set-DnsClientServerAddress -InterfaceAlias 'Ethernet' -ServerAddresses '10.0.0.4'
-"@
-Run-AzVMCommand -resourceGroup 'rg-lit-ADLab-ukw' -vmName 'lon-dc2' -script $script
-
-# Promote lon-dc2 as additional DC in learnitlessons.com
-$script = @"
-Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
-Import-Module ADDSDeployment
-`$securePassword = ConvertTo-SecureString '$password' -AsPlainText -Force
-`$cred = New-Object System.Management.Automation.PSCredential ("LIT\shumi", `$securePassword)
-Install-ADDSDomainController -NoGlobalCatalog:`$false -CreateDnsDelegation:`$false -Credential `$cred -CriticalReplicationOnly:`$false -DatabasePath 'C:\Windows\NTDS' -DomainName 'learnitlessons.com' -InstallDns:`$true -LogPath 'C:\Windows\NTDS' -NoRebootOnCompletion:`$false -SysvolPath 'C:\Windows\SYSVOL' -Force:`$true -SafeModeAdministratorPassword `$securePassword
-"@
-Run-AzVMCommand -resourceGroup 'rg-lit-ADLab-ukw' -vmName 'lon-dc2' -script $script
-
-Write-Host "Waiting for replication (10 minutes)..."
-Start-Sleep -Seconds 600
 
 # Configure pagefile and restart ams-dc1
 Configure-PagefileAndRestart -resourceGroup 'rg-lit-ADLab-weu' -vmName 'ams-dc1'
